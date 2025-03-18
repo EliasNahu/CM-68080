0fb12f5d653aa280d3829ae5f57dbdabda86353d
Elías <Elias.nahuel.1996@gmail.com>
2025-03-17 21:21:29 -0300

    Creacion de listas

--- main.js
+++ main.js
@@ -1,24 +1,42 @@
-function login(){
-    alert("Bienvenido al sistema")
-    let intentos = 1
-    let identificar = true
-    do{
-        let usuario = prompt("Ingresa tu usuario")
-        if(usuario === null){
-            break
-        }
-        if(usuario ==="elias" && intentos <=3){
-            alert("Bienvenido Elias")
-            identificar = false
-        }else{
-            alert("no reconozco el usuario")
-            intentos++
-            if(intentos > 3 ){
-                alert("superaste los 3 intentos disponibles")
-                break
-            }
-        }
-    }while(identificar)
+const Producto = function( nombre,precio,stock){
+    this.nombre = nombre,
+    this.precio = precio,
+    this.stock = stock
+}
+let producto1= new Producto("Harina",1300,30)
+let producto2= new Producto("Arroz",800,30)
+let producto3= new Producto("Fideos moñitos",2000,30)
+let producto4= new Producto("Fuyi",2400,20)
+let producto5= new Producto("Sedal (shampoo)",1600,25)
+let producto6= new Producto("Sedal (acondicionador)",1600,25)
+
+const lista = [producto1,producto2,producto3,producto4,producto5,producto6]
+
+//filtrar los productos para poder modificarlos
+
+function filtrarProductos(){
+    let palabraClave = prompt("ingresa el producto que buscas").trim().toUpperCase()
+    let resultado = lista.filter( (x)=> x.nombre.toUpperCase().includes(palabraClave)  )
+
+    if(resultado.length >0){ // si es mayor a 0, significa que al menos un resultado encontro
+        console.table(resultado)
+    }else{
+        alert("no se encontraron coincidencias en la base")
+    }
 }
 
-login()
+function agregarProducto(){
+
+    let nombre = prompt("ingresa el nombre del producto").trim();
+    let precio = parseFloat(prompt("ingresa el precio del producto"));
+    let stock = parseInt(prompt("ingresa el stock del producto"))
+
+    if(isNaN(precio) || isNaN(stock) || nombre === ""){
+    alert("por favor ingrese datos validos")
+    return;
+    }
+
+    let producto = new Producto(nombre,precio,stock)
+    lista.push(producto)
+    console.table(lista)
+}
